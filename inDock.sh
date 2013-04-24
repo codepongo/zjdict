025a0672e67b2fb2b71116f9d7c8eca192bd499d
codepongo <zhuhuotui@gmail.com>
2013-04-25 00:27:03 +0800

    inDock.sh add launcher in Mac Dock

---
+++ inDock.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/zjdict/zjdictcui.sh</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
+
+killall -HUP Dock
