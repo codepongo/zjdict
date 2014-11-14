f59d1e82ee57297071920f2253686c98fe067c1e
unknown <zhuhuotui@gmail.com>
2014-11-14 15:56:47 +0800

    refactor sound.py and add sound file from the bing website

--- sound.py
+++ sound.py
@@ -3,25 +3,61 @@ import urllib2
 import re
 import os
 import mp3play
+import socket
 sound_dir = 'sound'
-def soundByShanbay(w):
-    if not os.path.exists(sound_dir):
-        os.mkdir(sound_dir)
+def download(url):
+#    try:
+#        import proxy
+#        url = 'http://%s:%s' % (proxy.ip, proxy.port)
+#        proxy = urllib2.ProxyHandler({'http:':url})
+#        opener = urllib2.build_opener(proxy)
+#        urllib2.install_opener(opener)
+#    except ImportError, e:
+#        pass
     mp3_path = os.path.join(sound_dir, w + '.mp3')
-    if not os.path.isfile(mp3_path):
-        url = 'http://media.shanbay.com/audio/us/%s.mp3' % (w)
-        conn = urllib2.urlopen(url)
-        with open(os.path.join(sound_dir, w + '.mp3'), 'wb') as f:
-            f.write(conn.read())
-    clip = mp3play.load(mp3_path)
+    try:
+        if not os.path.isfile(mp3_path):
+            conn = urllib2.urlopen(url)
+            with open(os.path.join(sound_dir, w + '.mp3'), 'wb') as f:
+                f.write(conn.read())
+        return mp3_path
+    except socket.error, e:
+        if e.errno == 10054:
+            return ''
+        raise e
+def play(f):
+    clip = mp3play.load(f)
     clip.play() 
     # Let it play for up to 30 seconds, then stop it.
     import time
     time.sleep(clip.seconds())
     clip.stop()
 
+def sound(w):
+    if not os.path.exists(sound_dir):
+        os.mkdir(sound_dir)
+    urls = [
+        'http://media.shanbay.com/audio/us/%s.mp3' % (w),
+        'http://media.engkoo.com:8129/en-us/%s.mp3' % (w),
+    ]
+    for url in urls:
+        f = download(url)
+        if f != '':
+            play(f)
+            return
+
+
 if __name__ == '__main__':
     import sys
     w = sys.argv[1].decode(sys.stdin.encoding).encode('gbk')
-    soundByShanbay(w)
-        
+    urls = [
+        'http://media.shanbay.com/audio/us/%s.mp3' % (w),
+        'http://media.engkoo.com:8129/en-us/%s.mp3' % (w),
+    ]
+    for url in urls:
+        f = download(url)
+        if f != '':
+            print url, '->', f
+            play(f)
+        else:
+            print 'fail to download ', url
